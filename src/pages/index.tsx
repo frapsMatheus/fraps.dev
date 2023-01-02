253084e4c30134febcbafc0be606c3be39def727
Matheus Rosendo Pedreira <matrpedreira@gmail.com>
2023-01-02 16:05:22 -0300

    update(workplace): Update company name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className={styles.card}>
         <h1>Matheus Rosendo Pedreira, aka Fraps</h1>
         <h2>
-          Software Engineer @ <a href="http://codename.app/">CODENAME</a>
+          Software Engineer @ <a href="https://hotshot.xyz/">HOTSHOT</a>
         </h2>
         <h3>
           Computer Engineer that likes to try new things in the spare time
@@ -22,16 +22,14 @@ export default function Home() {
   function buttons() {
     return (
       <div className={styles.buttons}>
+        <a href="https://www.notion.so/frapsmatheus/Minha-lista-imposs-vel-d26409c03a814811bcb7c842d575a458">
+          IMPOSSIBLE LIST
+        </a>
         <a href="https://github.com/frapsMatheus">GITHUB</a>
         <a href="https://www.linkedin.com/in/matheus-rosendo-pedreira-65995698/">
           LINKEDIN
         </a>
-        {/* <a>IMPOSSIBLE LIST</a>
-        <a>BLOG</a> */}
         <a href="https://www.instagram.com/fraps_matheus">INSTAGRAM</a>
-        <a href="https://www.notion.so/frapsmatheus/Minha-lista-imposs-vel-d26409c03a814811bcb7c842d575a458">
-          IMPOSSIBLE LIST
-        </a>
       </div>
     );
   }
