af7b57de9f50bbcb11dabe4acab51b78546852e8
Matheus Rosendo Pedreira <matrpedreira@gmail.com>
2024-08-14 12:46:12 -0300

    feat: add ngrok redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/ngrok', // The path you want to redirect from
+        destination: 'https://master-actual-ocelot.ngrok-free.app', // The URL you want to redirect to
+        permanent: true, // Set to true for a 301 (permanent) redirect, or false for a 302 (temporary) redirect
+      },
+    ]
+  },
 };
