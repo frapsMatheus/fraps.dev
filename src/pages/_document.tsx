74df4fe1ae786fc0a1c4ad640824041d168222bc
frapsMatheus <matrpedreira@gmail.com>
2026-06-01 16:47:12 -0300

    feat: add Google AdSense verification and update work position

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-1164099775703792" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className={styles.card}>
         <h1>Matheus Rosendo Pedreira, aka Fraps</h1>
         <h2>
-          Software Engineer @ <a href="https://l1.co/">L1 Advisors Inc.</a>
+          Staff Software Engineer @ <a href="https://www.suaquadra.com.br/">Sua Quadra</a>
         </h2>
         <h3>Computer Engineer that likes to try new things in the spare time</h3>
       </div>
